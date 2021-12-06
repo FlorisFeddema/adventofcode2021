c61d24b8ad86ab2a9075ad9197c9f338fdecb46f
Floris <Floris1996@hotmail.com>
2021-12-06 13:44:25 +0100

    Fix part 2 day 5

--- day 5/part 2/run.go
+++ day 5/part 2/run.go
@@ -8,16 +8,16 @@ import (
 )
 
 type line struct {
-	x1 int
-	x2 int
-	y1 int
-	y2 int
+	startX int
+	startY int
+	endX   int
+	endY   int
 }
 
 func main() {
 	result := 0
 
-	buf, err := ioutil.ReadFile("data2.txt")
+	buf, err := ioutil.ReadFile("data.txt")
 	if err != nil {
 		panic(err)
 	}
@@ -32,112 +32,77 @@ func main() {
 
 		coordinates := strings.Split(e, " -> ")
 
-		x := strings.Split(coordinates[0], ",")
-		y := strings.Split(coordinates[1], ",")
+		start := strings.Split(coordinates[0], ",")
+		end := strings.Split(coordinates[1], ",")
 
-		x1, _ := strconv.Atoi(x[0])
-		x2, _ := strconv.Atoi(x[1])
-		y1, _ := strconv.Atoi(y[0])
-		y2, _ := strconv.Atoi(y[1])
+		startX, _ := strconv.Atoi(start[0])
+		startY, _ := strconv.Atoi(start[1])
+		endX, _ := strconv.Atoi(end[0])
+		endY, _ := strconv.Atoi(end[1])
 
-		line := line{x1, x2, y1, y2}
+		line := line{startX, startY, endX, endY}
 
 		lines = append(lines, line)
 	}
 
-	grid := [10][10]int{}
+	grid := [1000][1000]int{}
 
 	for _, e := range lines {
-		//vertical lines
-		if e.x2 == e.y2 {
-			//get highest
-			high := 0
-			low := 0
-			if e.x1 > e.y1 {
-				high = e.x1
-				low = e.y1
+		//horizontal
+		if e.startY == e.endY {
+
+			start, end := 0, 0
+
+			if e.startX > e.endX {
+				start = e.endX
+				end = e.startX
 			} else {
-				high = e.y1
-				low = e.x1
+				end = e.endX
+				start = e.startX
 			}
 
-			for i := 0; i <= (high - low); i++ {
-				grid[low+i][e.x2]++
-			}
-
-		}
-		//horizonal lines
-		if e.x1 == e.y1 {
-			//get highest
-			high := 0
-			low := 0
-			if e.x2 > e.y2 {
-				high = e.x2
-				low = e.y2
-			} else {
-				high = e.y2
-				low = e.x2
-			}
-
-			for i := 0; i <= (high - low); i++ {
-				grid[e.x1][low+i]++
+			for i := start; i <= end; i++ {
+				grid[e.startY][i]++
 			}
 		}
-		//diagonal lines
-		if e.x1 != e.y1 && e.x2 != e.y2 {
-			//get highest
-			start1 := 0
-			end1 := 0
-			start2 := 0
-			end2 := 0
-			if e.x2 > e.y2 {
-				start1 = e.x2
-				end1 = e.y2
+		//vertical
+		if e.startX == e.endX {
 
-				start2 = e.x1
-				end2 = e.y1
+			start, end := 0, 0
+
+			if e.startY > e.endY {
+				start = e.endY
+				end = e.startY
 			} else {
-				start1 = e.y2
-				end1 = e.x2
-
-				start2 = e.y1
-				end2 = e.x1
+				end = e.endY
+				start = e.startY
 			}
 
-			fmt.Print("Start ")
-			fmt.Print(start1)
-			fmt.Print(", ")
-			fmt.Println(end1)
-			fmt.Print("End ")
-			fmt.Print(start2)
-			fmt.Print(", ")
-			fmt.Println(end2)
+			for i := start; i <= end; i++ {
+				grid[i][e.startX]++
+			}
+		}
+		//diagonal
+		if e.endX != e.startX && e.endY != e.startY {
 
-			negative := false
-
-			if start2 > end2 {
-				negative = true
+			lenght := e.startX - e.endX
+			if lenght < 0 {
+				lenght *= -1
 			}
 
-			if start1 > end1 {
-				for i := 0; i <= (start1 - end1); i++ {
-					if negative {
-						grid[start1-i][start2-i]++
-					} else {
-						grid[start1-i][start2+i]++
-					}
-				}
-			} else {
-				for i := 0; i <= (start1 - end1); i++ {
-					if negative {
-						grid[start1+i][start2-i]++
-					} else {
-						grid[start1+i][start2+i]++
-					}
-				}
+			modX, modY := 1, 1
+
+			if e.startX > e.endX {
+				modX = -1
 			}
 
-			end2++
+			if e.startY > e.endY {
+				modY = -1
+			}
+
+			for i := 0; i <= lenght; i++ {
+				grid[e.startY+(i*modY)][e.startX+(i*modX)]++
+			}
 		}
 	}
 
